3c3880fa2a3606f43dbb675b10b6cee780fb384b
ankit <ankit@gmail.com>
2021-10-30 17:33:33 +0530

    modified

--- action2.php
+++ action2.php
@@ -12,15 +12,22 @@
   	$res_u = mysqli_query($db, $sql_u);
 
   	if (mysqli_num_rows($res_u) > 0) {
-  	  echo "Sorry... username already taken"; 	
+  	  echo '<script>
+          alert("username already exist");
+          window.location="index.php";
+      </script>';
+      exit();	
   		
   	}else{
            $query = "INSERT INTO users (username, password) 
       	    	  VALUES ('$username','".md5($password)."')";
            $results = mysqli_query($db, $query);
+           $_SESSION['username'] = $username;
 		   
-           $_SESSION['username'] = $results['username'];
-           header("Location: home.php");
+          echo '<script>
+          
+          window.location="home.php";
+          </script>';
            exit();
            
   	}
--- home.php
+++ home.php
@@ -5,7 +5,7 @@ if (isset($_POST['register'])) {
    header("Location: index.php");
 }
 
- ?>
+ ?> 
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,12 +15,39 @@ if (isset($_POST['register'])) {
                margin:0%;
                padding:0%;
                background:skyblue;
+               text-align: center;
+               
+               
           }
+          #body{
+               
+               width:800px; 
+               margin:auto;
+               
+          }
+          button{
+          
+               background-color: white;
+               border-radius: 10px;
+                width: 100px;
+              height: 30px;
+              font-size:13px;
+              font-weight: bold;
+          }
+          button:hover{
+    background-color:yellow;}
+         
      </style>
 </head>
 <body>
-     <h1>Hello, <?php echo $_SESSION['username']; ?></h1>
-     <a href="logout.php">Logout</a>
+     <div id="body">  
+     <h1>Hello <?php echo $_SESSION['username']; ?></h1>
+     <button><a href="logout.php">Logout</a></button>
+      </div>
+    
+
+
+     </script>
 </body>
 </html>
 
